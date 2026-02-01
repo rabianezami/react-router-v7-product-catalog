7ddd3d1d3471b7559de9d9a2cb7c127fa1ddae00
rabia zia nezami <rabianezami786@gail.com>
2026-02-01 19:26:15 +0430

    feat(home): Add Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <section className="max-w-4xl mx-auto bg-gray-50">
+            <div className="px-4 py-50 text-center">
+                <h1 className="text-4xl font-bold mb-4 text-gray-700">Welcome to Your Product Catalog</h1>
+                <p className="text-md text-gray-600">This is a simple wep applicaton that will use Routes, Params, Navigation...</p>
+            </div>
+        </section>
+    )
+}
