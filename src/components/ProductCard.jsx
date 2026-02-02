b6d514386868c2c17db5510fb2a53c68b39e6834
rabia zia nezami <rabianezami786@gail.com>
2026-02-02 23:14:22 +0430

    fix(prop): change title to name

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,11 +5,12 @@ export default function ProductCard({ product }) {
         <section className="border border-gray-100 rounded shadow hover:shadow-lg transition p-3 mx-5">
                 <img
                     src={product.image}
-                    alt={product.title}
+                    alt={product.name}
                     className="w-full h-40 object-cover rounded mb-2"
                 />
                 <div className="pl-5">
-                    <h2 className="font-semibold text-lg">{product.title}</h2>
+                    <h2 className="font-semibold text-lg">{product.name}</h2>
+                    <p className="text-sm text-gray-500">{product.category}</p>
                     <p className="text-gray-600">{product.price}</p>
                     <div className="flex justify-center mt-2">
                         <Link
