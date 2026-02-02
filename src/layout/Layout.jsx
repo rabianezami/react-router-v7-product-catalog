dea455d5357569fe4b4a604c51c71a64f2cd393e
rabia zia nezami <rabianezami786@gail.com>
2026-02-02 21:56:37 +0430

    fix(props): send links prop to layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,10 @@
 import { NavLink, Outlet } from "react-router-dom";
-import Navbar from "..components/Navbar";
+import Navbar from "../components/Navbar";
 
-export default function Layout({ isAuth }) {
+export default function Layout({ isAuth, links}) {
     return (
         <div>
-            <Navbar isAuth={isAuth} links={NavLink}/>
+            <Navbar isAuth={isAuth} links={links}/>
             <main className="max-w-6xl mx-auto px-4 py-4">
                 <Outlet />
             </main>
