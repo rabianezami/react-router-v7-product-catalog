f0cf437a88180ce45fd5c71e91b5fe49a1585b63
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-02 22:01:42 +0430

    feat(dashboard): Add Dashboard component

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+        <div className="text-center">
+            <h1>Dashboard</h1>
+            <p className="text-gray-600">This is a protect route</p>
+        </div>
+    )
+}
