4deb4ad5d373d859d249c6cb549b82f5e4d75dc2
rabia zia nezami <rabianezami786@gail.com>
2026-02-02 21:58:14 +0430

    feat(productDetails): Add ProductDetails component

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,29 @@
+import { useNavigate, useParams } from "react-router-dom"
+import { getProduct } from "../data/data"
+
+export default function ProductDetails() {
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const product = getProduct(id)
+
+    if (!product) {
+        return <p>Product Not found!</p>
+    }
+
+    return (
+        <div className="max-w-xl mx-auto border border-gray-200 shadow rounded p-6 mt-10">
+            <h1 className="font-bold text-3xl mb-2">
+                {product.title}
+            </h1>
+            <p className="text-gray-600"> Product ID: {id}</p>
+
+            <button
+                className="bg-gray-200 px-4 py-2 rounded hover:bg-gray-300 mt-4"
+                onClick={() => navigate(-1)}
+            >
+                ⬅ Back
+            </button>
+        </div> 
+    )
+}
