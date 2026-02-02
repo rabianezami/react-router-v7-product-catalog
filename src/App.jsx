3138730f364576401d0951239ddc7a567a2f0e41
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-02 22:43:23 +0430

    fix: Defines ProductLayout and fix childern route

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,43 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import NotFound from "./pages/NotFound";
+import ProductLayout from "./pages/ProductsLayout";
 
 const navLinks = [
-    {name: "Home", path: "/"},
-    {name: "Products", path: "/products"},
-    {name: "Dashboard", path: "/dashboard"},
+    { name: "Home", path: "/" },
+    { name: "Products", path: "/products" },
+    { name: "Dashboard", path: "/dashboard" },
 ]
 
 export default function App() {
-   const [isAuth, setIsAuth] = useState(false)
+    const [isAuth, setIsAuth] = useState(false)
 
-   function onLogin() {
-      setIsAuth(true)
-   }
+    function onLogin() {
+        setIsAuth(true)
+    }
 
-   return (
-    <Routes>
-        <Route element={<Layout isAuth={isAuth} links={navLinks}/>}>
-           <Route path="/" element={<Home />} />
-           <Route path="products" element={<ProductList />} />
-           <Route path="products/:id" element={<ProductDetails />} />
-           <Route path="login" element={<Login onLogin={onLogin} />} />
+    return (
+        <Routes>
+            <Route element={<Layout isAuth={isAuth} links={navLinks} />}>
+                <Route path="/" element={<Home />} />
 
-           <Route path="/dashboard" element={
-            <ProtectedRoute isAuth={isAuth}>
-                <Dashboard />
-            </ProtectedRoute>
-           }
-           />
+                <Route path="products" element={<ProductLayout />}>
+                    <Route index element={<ProductList />} />
+                    <Route path=":id" element={<ProductDetails />} />
+                </Route>
 
-           <Route path="*" element={<NotFound />}/>
-        
-        </Route>
-    </Routes>
-   )
+                <Route path="login" element={<Login onLogin={onLogin} />} />
+
+                <Route path="/dashboard" element={
+                    <ProtectedRoute isAuth={isAuth}>
+                        <Dashboard />
+                    </ProtectedRoute>
+                }
+                />
+
+                <Route path="*" element={<NotFound />} />
+
+            </Route>
+        </Routes>
+    )
 }
