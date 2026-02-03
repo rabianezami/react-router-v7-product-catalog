b88627085e5ec71825e562c491d2a6356f14c75c
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-03 13:44:44 +0430

    styles: improve notFound product design

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { getProduct } from "../data/data"
 
 export default function ProductDetails() {
@@ -8,7 +8,20 @@ export default function ProductDetails() {
     const product = getProduct(id)
 
     if (!product) {
-        return <p>Product Not found!</p>
+        <div className="text-center mt-10">
+            <h2 className="text-xl font-semibold mb-2">
+                Product not found
+            </h2>
+            <p className="text-gray-600 mb-4">
+                The product you are looking not exist.
+            </p>
+            <Link
+                to="/products"
+                className="underline"
+            >
+                Back to Products
+            </Link>
+        </div>
     }
 
     return (
@@ -16,7 +29,18 @@ export default function ProductDetails() {
             <h1 className="font-bold text-3xl mb-2">
                 {product.name}
             </h1>
-            <p className="text-gray-600">{product.description}</p>
+
+            <p className="text-gray-700 mb-1">
+                <strong>Price:</strong> {product.price}
+            </p>
+
+            <p className="text-gray-700 mb-1">
+                <strong>Category:</strong> {product.category}
+            </p>
+
+            <p className="text-gray-600 mt-3">
+                {product.description}
+            </p>
 
             <button
                 className="bg-gray-200 px-4 py-2 rounded hover:bg-gray-300 mt-4"
@@ -24,6 +48,6 @@ export default function ProductDetails() {
             >
                 ⬅ Back
             </button>
-        </div> 
+        </div>
     )
 }
